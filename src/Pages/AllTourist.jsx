912c659a2c4c2911fbf4984aca24a409f5d68a08
hbmubin <hasanulbannamubin01@gmail.com>
2024-04-30 14:27:14 +0600

    srotBy-added

--- src/Pages/AllTourist.jsx
+++ src/Pages/AllTourist.jsx
@@ -1,20 +1,53 @@
 import { useLoaderData } from "react-router-dom";
 import SpotCard from "./SpotCard";
+import { useState } from "react";
 
 const AllTourist = () => {
   const spots = useLoaderData();
+  const [sortedSpots, setSortedSpots] = useState([...spots]);
+
+  const handleSortBy = (filter) => {
+    if (filter === "ascending") {
+      const sorted = [...spots].sort((a, b) => b.cost - a.cost);
+      setSortedSpots(sorted);
+    }
+    if (filter === "descending") {
+      const sorted = [...spots].sort((a, b) => a.cost - b.cost);
+      setSortedSpots(sorted);
+    }
+  };
 
   return (
-    <div className="mt-10">
-      {spots ? (
-        <div className="grid grid-cols-2 gap-6">
-          {spots.map((spot) => (
-            <SpotCard key={spot._id} spot={spot}></SpotCard>
-          ))}
+    <div>
+      <div className="text-center">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn m-1 rounded-3xl">
+            Sort by Cost
+          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-3xl"
+          >
+            <li>
+              <a onClick={() => handleSortBy("ascending")}>Ascending</a>
+            </li>
+            <li>
+              <a onClick={() => handleSortBy("descending")}>Descending</a>
+            </li>
+          </ul>
         </div>
-      ) : (
-        <div> No data to show</div>
-      )}
+      </div>
+      <div className="mt-10">
+        {spots ? (
+          <div className="grid grid-cols-2 gap-6">
+            {sortedSpots.map((spot) => (
+              <SpotCard key={spot._id} spot={spot}></SpotCard>
+            ))}
+          </div>
+        ) : (
+          <div> No data to show</div>
+        )}
+      </div>
     </div>
   );
 };
